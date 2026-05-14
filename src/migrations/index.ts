54fc0ae23a9c32c2fc8c9cb5c1cdd17db8081d9d
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2026-05-14 15:44:38 +0200

    setup

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260514_134157 from './20260514_134157';
+
+export const migrations = [
+  {
+    up: migration_20260514_134157.up,
+    down: migration_20260514_134157.down,
+    name: '20260514_134157'
+  },
+];
